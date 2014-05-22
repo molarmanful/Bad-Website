c32b88c0c69c9d54fd628c6c774238ecdef54f77
molarmanful <molarmanful@users.noreply.github.com>
2014-05-21 19:12:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-
+  var x;
+  function changecolors() {
+      x = 1;
+      setInterval(change, 250);
+  }
+  function change() {
+      if (x === 1) {
+          bcolor = "red";
+          color = "blue";
+          x = 2;
+      } else {
+          bcolor = "black";
+          bcolor = "red"
+          x = 1;
+      }
+      document.getElementbyId('flash').style.backgroundColor = bcolor;
+      document.getElementbyId('flash').style.backgroundColor = color;
+  }
 });
